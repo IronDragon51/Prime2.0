b238d1c9e3df6782981a387d479a6e126417dd9f
Cyb3rDargon <matepapp00@gmail.com>
2023-04-03 09:42:17 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿class Result
+{
+    public static bool satisfy = true;
+    public static int primeDigitSums(int n)
+    {
+        int count = 0;
+        long min = (long)Math.Pow(10, (n - 1)) + 1;
+        long max = ((long)Math.Pow(10, n)) - 1;
+
+        for (long currentNumber = min; currentNumber <= max; currentNumber++)
+        {
+            satisfy = true;
+
+            int numberOfDigits = 3;
+            SatisfyTheRule(numberOfDigits, currentNumber, n);
+            if (!satisfy)
+            {
+                continue;
+            }
+
+            numberOfDigits = 4;
+            SatisfyTheRule(numberOfDigits, currentNumber, n);
+            if (!satisfy)
+            {
+                continue;
+            }
+
+            numberOfDigits = 5;
+            SatisfyTheRule(numberOfDigits, currentNumber, n);
+            if (!satisfy)
+            {
+                continue;
+            }
+
+            if (satisfy)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void SatisfyTheRule(int numberOfDigits, long currentNumber, int n)
+    {
+        if (satisfy)
+        {
+            for (int j = 0; j + (numberOfDigits - 1) < n; j++)
+            {
+                int sumDigits = currentNumber.ToString().Substring(j, numberOfDigits).Sum(c => c - '0');
+
+                satisfy = IsPrime(sumDigits);
+                if (!satisfy)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+
+    public static bool IsPrime(int sumDigits)
+    {
+        if (sumDigits % 2 == 0) return false;
+        if (sumDigits == 2) return true;
+        if (sumDigits <= 1) return false;
+
+        var boundary = (int)Math.Floor(Math.Sqrt(sumDigits));
+
+        for (int i = 3; i <= boundary; i += 2)
+            if (sumDigits % i == 0)
+                return false;
+
+        return true;
+    }
+
+
+    public static bool[] FillIsPrimeArray()
+    {
+        bool[] isPrime = new bool[100001];
+
+        for (int i = 100; i < isPrime.Length; i++)
+        {
+            isPrime[i] = IsPrime(i);
+        }
+
+        return isPrime;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.primeDigitSums(n);
+
+            Console.WriteLine(result);
+        }
+
+    }
+}
