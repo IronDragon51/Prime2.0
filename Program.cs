2a18d4d2941ffad03d862cb32f8b2626e9a3b8ee
Cyb3rDargon <matepapp00@gmail.com>
2023-04-03 09:42:53 +0200

    original

--- Program.cs
+++ Program.cs
@@ -75,18 +75,6 @@
     }
 
 
-    public static bool[] FillIsPrimeArray()
-    {
-        bool[] isPrime = new bool[100001];
-
-        for (int i = 100; i < isPrime.Length; i++)
-        {
-            isPrime[i] = IsPrime(i);
-        }
-
-        return isPrime;
-    }
-
 }
 
 class Solution
